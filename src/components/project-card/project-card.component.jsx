d057cf5a2f8b2c341cd51825a6be1ca3aa2d01e8
Anastasia Reed <lindsay@Anastasias-Air.attlocal.net>
2022-07-25 20:34:18 -0500

    updated scss on project cards and added project descriptions updated scss on the employee cards

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -1,4 +1,4 @@
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./project-card.styles.scss";
 
@@ -8,29 +8,36 @@ const ProjectCard = () => {
       id: 1,
       title: "Mock Employee Directory",
       nav: "/employee-lookup",
+      description:
+        "Meant for internal use, ideally, an intranet page like this would pull the data from the ERP or accounting software. For this project the fake-data is from JSONPlaceholder.typicode.com. It's a riff on one of the first React projects I completed called 'Monster's Rolodex.'",
     },
     {
       id: 2,
       title: "Mock Landing Page",
       nav: "/landing-page",
+      description:
+        "I used Material UI components for the text field and button.  The text field checks for a valid email address with error feedback on the page.  The button currently clears the text field.",
     },
     {
       id: 3,
-      title: "Project 3",
+      title: "Comming Soon...",
       nav: "/project03",
+      description: "",
     },
     // {
     //   id: 4,
     //   title: "Project 4",
+    // description: "",
     // },
   ];
   return (
-    <div className="project-card-container" key={projects.id}>
-      {projects.map(({ title, nav }) => (
-        <div className="project-card">
+    <div className="project-card-container">
+      {projects.map(({ id, title, nav, description }) => (
+        <div className="project-card" key={id}>
           <div className="project-title">
             <h2>{title}</h2>
             <Link to={nav}>Click here</Link>
+            <p>{description}</p>
           </div>
         </div>
       ))}
